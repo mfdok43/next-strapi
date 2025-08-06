0cf3fd4225f8ec633aaeb32a8d23759a25553b1c
Maksym Mudrak <mfdok4388@gmail.com>
2025-08-06 03:42:57 +0300

    first commit 2

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    // keys: env.array('APP_KEYS'),
+    keys: ["qE8TAuqOBPkYmjSBqzQgvw==","dUogiw6EU90QohZqKO6Vfw==","7coV/OkhKEkmEE40TtaqOQ==","2w5rhcONBQJEq8rjA4wC6Q=="]
   },
 });
